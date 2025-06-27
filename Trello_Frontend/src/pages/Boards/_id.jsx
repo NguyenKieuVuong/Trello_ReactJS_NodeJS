aabc3cdb42e5afffb0356cf79cd735f77a69042c
Vuong Nguyen <nguyenkieuvuong@gmail.com>
2025-06-27 23:37:36 +0700

    link absolute relative

--- Trello_Frontend/src/pages/Boards/_id.jsx
+++ Trello_Frontend/src/pages/Boards/_id.jsx
@@ -1,6 +1,6 @@
 //Board Details chi tiết của page nay loc theo id
 import Container from "@mui/material/Container";
-import AppBar from "../../components/AppBar";
+import AppBar from "~/components/AppBar";
 import BoardBar from "./BoardBar";
 import BoardContent from "./BoardContent";
 function Board() {
